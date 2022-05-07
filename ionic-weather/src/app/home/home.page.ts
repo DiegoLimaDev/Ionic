d5ba5fc100dbb7c21a7fee53329aed1e85a00f60
Diego Lima <xxdiegolsxx@gmail.com>
2022-05-07 20:26:08 -0300

    Initial

--- ionic-weather/src/app/home/home.page.ts
+++ ionic-weather/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
 
 @Component({
@@ -6,8 +6,8 @@ import { WeatherService } from '../services/weather.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  private res;
+export class HomePage implements OnInit {
+  public res;
 
   constructor(
     private weatherApi: WeatherService) {}
